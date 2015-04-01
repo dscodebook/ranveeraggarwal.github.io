caef06268e333382056ad911021455c2eb9fbfc0
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2015-04-01 23:41:45 +0530

    Full revamp

--- website/donations/banner.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $.getJSON('http://planetkde.org/donations/get.php', function(data) {
-        $('#donation-goal-amt').html(data.goal);
-        $('#donation-bar').css('width', data.pcnt + '%');
-    });
-});
